bb0d181ae6512d69931b7e30a78a009e0b7355a0
Scott-fo <scottfo@live.com>
2022-12-16 00:35:31 +0000

    fix: add missing cors

--- gateway/server.ts
+++ gateway/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { router } from "./src/routes/routes";
+import cors from "cors";
 
 const PORT = 8000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, "0.0.0.0", () => {
